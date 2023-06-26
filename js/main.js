9bbc760d9a37d040845add89f5740c23b180724f
Bidhya Khadka <kdkbidhya248@gmail.com>
2023-06-26 11:32:47 +0545

    portfolio using bootstrap

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// nav bar hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach((a) => {
+  a.addEventListener("click", () => {
+    navCollapse.classList.remove("show");
+  });
+});
